428cae209368b95bfef0f0457c79e73c53fe97c8
Corey Porter <cp@253n.org>
2025-12-01 11:35:04 -0300

    2025 day 1 part 2

--- 2025/day1.cpp
+++ 2025/day1.cpp
@@ -32,6 +32,11 @@ std::optional<int> turn_from_string(const std::string &s) {
     };
 }
 
+// we only want numbers - (LOCK_SIZE-1) < x < (LOCK_SIZE-1),
+// but before we mod our turns figure out how many full turns
+// we go through otherwise for counting the times we pass zero.
+int full_turns(int x) { return std::abs(x / 100); }
+
 int main(int, char **) {
     auto collectedOpt
         = prelude::collect_optional(prelude::line_view(std::cin) | rv::transform(turn_from_string));
@@ -44,7 +49,13 @@ int main(int, char **) {
 
     int position = 50;
     int part1 = 0;
+    int part2 = 0;
     for (auto &x : turns) {
+        part2 += full_turns(x);
+        x %= LOCK_SIZE;
+        if (position != 0 && (position + x > LOCK_SIZE || position + x < 0)) {
+            ++part2;
+        }
         position = lock_mod(position + x);
         if (position == 0) {
             ++part1;
@@ -52,6 +63,7 @@ int main(int, char **) {
     }
 
     fmt::print("part 1: {}\n", part1);
+    fmt::print("part 2: {}\n", part1 + part2);
 
     return 0;
 }
